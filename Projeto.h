d442a134dc35e6376cd4cc9ea680e65542f1f26e
JPLopes05 <149200225+JPLopes05@users.noreply.github.com>
2023-11-12 23:26:36 -0300

    Função alterar e status adicionado

--- Projeto.h
+++ Projeto.h
@@ -1,1 +1,19 @@
+#ifndef PROJETO_H
+#define PROJETO_H
 
+struct Tarefa {
+    int prioridade;
+    char categoria[100];
+    char descricao[300];
+    char status[100];
+};
+//Espaço responsável por definir a estrutura da tarefa
+
+void limpa();
+//cabeçalho da função responsável por limpar as informações salvas na variável para poder armazenar novas informações
+void ler(struct Tarefa *armazena);
+//cabeçalho da função que lê as informações dadas pelo usuário e armazena no arquivo
+void excluir_tarefa(struct Tarefa *tarefas, int *cont, int posicao);
+//cabeçalho da função que vai excluir as tarefas
+void alterar_tarefa(struct Tarefa *tarefas, int cont);
+#endif//PROJETO_H
